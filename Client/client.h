0205c725d806b11a2e4c278af5607a660df84167
hosseingeranfar <104078915+hosseingeranfar@users.noreply.github.com>
2026-02-18 05:41:21 +0330

    Update client.h

--- Client/client.h
+++ Client/client.h
@@ -3,6 +3,7 @@
 
 #include <QObject>
 #include <QLocalSocket>
+#include <QTimer>
 
 class Client : public QObject
 {
@@ -12,16 +13,21 @@ public:
     explicit Client(const QString &serverName, QObject *parent = nullptr);
     ~Client();
 
-    void sendMessage(const QString &message);
+    void connectToServer();             // فقط connect می‌کنه
+    void setMessage(const QString &message);  // پیام رو set می‌کنه
 
 private slots:
     void onConnected();
     void onReadyRead();
     void onDisconnected();
     void onError(QLocalSocket::LocalSocketError socketError);
+    void onConnectionTimeout();         // برای مدیریت timeout
 
 private:
+    void sendMessage();                 // فقط send می‌کنه (private)
+
     QLocalSocket *m_socket;
+    QTimer *m_connectionTimer;
     QString m_serverName;
     QString m_pendingMessage;
     QByteArray m_buffer;
